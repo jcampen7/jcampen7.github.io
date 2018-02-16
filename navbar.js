c322873b2618f0838c134806290c6a5cd719087e
jcampen7 <jcampen@icloud.com>
2018-02-16 14:04:37 -0600

    test 2

---
+++ navbar.js
@@ -0,0 +1,8 @@
+document.write('\
+<ul>\
+   <li><a href="homepage.htm">Home</a></li>\
+   <li><a href="info.htm">Info</a></li>\
+   <li><a href="resume.htm">Resume</a></li>\
+   <li><a href="projects.htm">Projects</a></li>\
+</ul>\
+');
---
+++ titlebar.js
@@ -0,0 +1,4 @@
+document.write('\
+<span class="lefttitle">jbcampen</span> <span class="midtitle">Jacob B Campen\
+</span> <span class="righttitle">ISU</span>\
+');
